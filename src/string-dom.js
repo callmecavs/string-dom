2dbf6687fc8a10ab7e4b33c35c42bde83000f062
callmecavs <callmecavs@gmail.com>
2017-02-09 21:00:38 -0500

    rewrite to remove currying

--- src/string-dom.js
+++ src/string-dom.js
@@ -1,20 +1,27 @@
 // the element creator function
-// curried to accept tag, then attributes and children
-const element = tag => (attrs = {}, ...children) => {
-  // process the attributes object into a string
-  // trim extra leading space
+// accepts a tag name, then (optionally) an attiributes object, then children
+const element = (tag, ...rest) => {
+  const secondArg = rest[0]
+
+  // catch elements without attributes
+  if (typeof secondArg !== 'object') {
+    return `<${tag}>${rest.join('')}</${tag}>`
+  }
+
+  // process the attributes object into a string, then trim the leading space
   const formatted = Object
-    .keys(attrs)
+    .keys(secondArg)
     .reduce((accum, current) => {
-      accum += ` ${current}="${attrs[current]}"`
+      accum += ` ${current}="${secondArg[current]}"`
       return accum
     }, '')
     .slice(1)
 
   // concatentate the children
-  // nested elements just return Strings,
-  // and regular text (not wrapped in an element) is a String too
-  const concatted = children.join('')
+  // nested function calls return Strings, and unwrapped text is a String too
+  const concatted = rest
+    .slice(1)
+    .join('')
 
   // return an HTML string representing the element
   return `<${tag} ${formatted}>${concatted}</${tag}>`
