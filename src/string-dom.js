ef93b6e9fc584b315ec81fc6b7ffa8cc15430f6f
callmecavs <callmecavs@gmail.com>
2017-02-10 18:23:13 -0500

    rewrite (more clarity imo)

--- src/string-dom.js
+++ src/string-dom.js
@@ -1,54 +1,55 @@
 import empty from './empty.js'
 
-// the element creator function
-// accepts a tag name, then (optionally) an attributes object, then children
 const element = (tag, ...rest) => {
-  // destructure what may or may not be the attributes object
-  const attrs = rest[0]
-
-  // check for component functions
-  // if component function, call it, passing it props
+  // check for component
   if (typeof tag === 'function') {
-    // check if the component is being passed props,
-    // otherwise initialize an empty set
+    // if no props, create empty set
     const props = rest[0] || {}
 
-    // pass children content to the component as a prop
-    props.children = rest
+    // add children to props
+    props['children'] = rest
       .slice(1)
       .join('')
 
-    // pass the updated props, including the children, to the component function
+    // call component function, passing props
     return tag(props)
   }
 
-  // check for elements without attributes
-  // the second parameter will be null (with JSX) or a string (without JSX)
-  if (attrs === null || typeof attrs !== 'object') {
-    return `<${tag}>${rest.join('')}</${tag}>`
+  let attrs
+  let children
+
+  const secondArg = rest[0]
+
+  // check for attributes
+  // children are processed based on their presence
+  if (secondArg !== null && typeof secondArg === 'object') {
+    attrs = Object
+      .keys(secondArg)
+      .reduce((accum, attr) => {
+        // if event, throw
+        if (attr.indexOf('on') === 0) {
+          throw new Error('string-dom: event binding is not supported.')
+        }
+
+        // note the leading space
+        accum += ` ${attr}="${secondArg[attr]}"`
+        return accum
+      }, '')
+
+    // remove attributes object before joining children
+    children = rest.slice(1).join('')
+  } else {
+    attrs = ''
+    children = rest.join('')
   }
 
-  // process the attributes object into a string, then trim the leading space
-  const formatted = Object
-    .keys(attrs)
-    .reduce((accum, current) => {
-      // throw for inline events added by JSX
-      if (current.indexOf('on') === 0) {
-        throw new Error(`string-dom: event handler unsupported: ${current}.`)
-      }
+  // empty tag
+  if (empty.indexOf(tag) !== -1) {
+    return `<${tag}${attrs}>`
+  }
 
-      accum += ` ${current}="${attrs[current]}"`
-      return accum
-    }, '')
-    .slice(1)
-
-  // concatentate the children
-  const concatted = rest
-    .slice(1)
-    .join('')
-
-  // return an HTML string representing the element
-  return `<${tag} ${formatted}>${concatted}</${tag}>`
+  // normal tag
+  return `<${tag}${attrs}>${children}</${tag}>`
 }
 
 export default element
