9803b486d65ca9180148a6c2ffde932a34054e25
Michael Cavalea <callmecavs@gmail.com>
2017-02-10 01:32:33 -0500

    add working components

--- src/string-dom.js
+++ src/string-dom.js
@@ -3,6 +3,22 @@
 const element = (tag, ...rest) => {
   const secondArg = rest[0]
 
+  // check for components
+  // call the function, passing it rest of the params
+  if (typeof tag === 'function') {
+    // check if the component is being passed props,
+    // otherwise initialize an empty set
+    const props = rest[0] || {}
+
+    // add children passed to the component as a prop
+    props.children = rest
+      .slice(1)
+      .join('')
+
+    // pass the props, including the children, to the component function
+    return tag(props)
+  }
+
   // check for elements without attributes
   // the second parameter will be null (with JSX) or a string (without JSX)
   if (secondArg === null || typeof secondArg !== 'object') {
