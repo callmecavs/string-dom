f6117858b82be3c2b94bf212f405a16b760a36b5
callmecavs <callmecavs@gmail.com>
2017-02-09 20:04:21 -0500

    one take jake

--- src/string-dom.js
+++ src/string-dom.js
@@ -1,9 +1,37 @@
-// need to capture children using the rest parameter
+const element = tag => (attrs = {}, ...children) => {
+  // process the attributes object into a string
+  // trim extra leading space
+  const formatted = Object
+    .keys(attrs)
+    .reduce((accum, current) => {
+      accum += ` ${current}="${attrs[current]}"`
+      return accum
+    }, '')
+    .slice(1)
 
-const element = tag => (attrs = {}) => {
+  // concatentate the children
+  // nested elements just return Strings,
+  // and regular text (not wrapped in an element) is a String too
+  const concatted = children.join('')
 
+  // return an HTML string representing the element
+  return `<${tag} ${formatted}>${concatted}</${tag}>`
 }
 
-// export a bunch of common tag functions?
+// DIV
 
-export default element
+const div = element('div')
+
+export { div }
+
+// H1
+
+const h1 = element('h1')
+
+export { h1 }
+
+// P
+
+const p = element('p')
+
+export { p }
