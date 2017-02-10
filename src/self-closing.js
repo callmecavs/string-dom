bfe527ad15a7428c1f7b5ad4bf1bbabb78a603cb
Michael Cavalea <callmecavs@gmail.com>
2017-02-10 11:51:50 -0500

    add list of self-closing tag names

---
+++ src/self-closing.js
@@ -0,0 +1,21 @@
+// borrowed from: https://github.com/developit/vhtml/blob/master/src/empty-tags.js
+const empty = [
+  'area',
+  'base',
+  'br',
+  'col',
+  'command',
+  'embed',
+  'hr',
+  'img',
+  'input',
+  'keygen',
+  'link',
+  'meta',
+  'param',
+  'source',
+  'track',
+  'wbr'
+]
+
+export default empty
