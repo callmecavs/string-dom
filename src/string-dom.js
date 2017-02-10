37bc0a467967a2bcb3b033fc9bdf9de869ed622a
Michael Cavalea <callmecavs@gmail.com>
2017-02-10 01:40:47 -0500

    renaming, comments, clarity

--- src/string-dom.js
+++ src/string-dom.js
@@ -1,38 +1,39 @@
 // the element creator function
 // accepts a tag name, then (optionally) an attributes object, then children
 const element = (tag, ...rest) => {
-  const secondArg = rest[0]
+  // destructure what may or may not be the attributes object
+  const attrs = rest[0]
 
-  // check for components
-  // call the function, passing it rest of the params
+  // check for component functions
+  // if component function, call it, passing it props
   if (typeof tag === 'function') {
     // check if the component is being passed props,
     // otherwise initialize an empty set
     const props = rest[0] || {}
 
-    // add children passed to the component as a prop
+    // pass children content to the component as a prop
     props.children = rest
       .slice(1)
       .join('')
 
-    // pass the props, including the children, to the component function
+    // pass the updated props, including the children, to the component function
     return tag(props)
   }
 
   // check for elements without attributes
   // the second parameter will be null (with JSX) or a string (without JSX)
-  if (secondArg === null || typeof secondArg !== 'object') {
+  if (attrs === null || typeof attrs !== 'object') {
     return `<${tag}>${rest.join('')}</${tag}>`
   }
 
   // process the attributes object into a string, then trim the leading space
   const formatted = Object
-    .keys(secondArg)
+    .keys(attrs)
     .reduce((accum, current) => {
       // TODO: throw for events added by JSX
       // if (current.indexOf('on') === 0)
 
-      accum += ` ${current}="${secondArg[current]}"`
+      accum += ` ${current}="${attrs[current]}"`
       return accum
     }, '')
     .slice(1)
