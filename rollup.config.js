d3582db19b89329b5f861cdbb9ab5dad4f245069
callmecavs <callmecavs@gmail.com>
2017-02-09 19:45:56 -0500

    outset-lib

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+
+const info = require('./package.json')
+
+const config = {
+  entry: 'src/string-dom.js',
+  plugins: [
+    resolve(),
+    babel()
+  ],
+  targets: [
+    {
+      dest: info.main,
+      format: 'umd',
+      moduleName: 'StringDom'
+    }, {
+      dest: info.module,
+      format: 'es'
+    }
+  ]
+}
+
+export default config
---
+++ src/string-dom.js
@@ -0,0 +1,9 @@
+// need to capture children using the rest parameter
+
+const element = tag => (attrs = {}) => {
+
+}
+
+// export a bunch of common tag functions?
+
+export default element
